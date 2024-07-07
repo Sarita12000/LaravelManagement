bfb58d1656131f296c073d4d56695ced312eb8cd
Sarita Chavan <saritachavan12000@gmail.com>
2024-07-07 19:32:54 +0530

    testing and demo method

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,11 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    public function test_method(){
+        echo 'testing';
+    }
+
+    public function demo(){
+        echo "demo";
+    }
 }
